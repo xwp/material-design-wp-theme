cbee443d62cab8b74e467ec44b8d0ad82fd7d7dd
Mario Aguiar <me@marioaguiar.net>
2020-03-16 10:14:47 -0600

    Set a basic webpack configuration

--- functions.php
+++ functions.php
@@ -122,6 +122,8 @@ add_action( 'widgets_init', 'material_theme_wp_widgets_init' );
 function material_theme_wp_scripts() {
 	wp_enqueue_style( 'material-theme-wp-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'material-theme-style', get_template_directory_uri() . '/assets/css/material-theme-style.css', array(), '20151215', true );
+
 	wp_enqueue_script( 'material-theme-wp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'material-theme-wp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = [{
+	entry: './assets/src/css/index.scss',
+	output: {
+		path: `${__dirname}/assets/dist`,
+		filename: 'style-bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'material-theme-style.css',
+							outputPath: 'css',
+						},
+					},
+					{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							implementation: require( 'sass' ),
+						},
+					},
+				]
+			}
+		]
+	},
+}];
