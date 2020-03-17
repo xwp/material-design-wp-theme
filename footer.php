ef745fc0af8129d810dbb139b46fe0952ced19af
Mario Aguiar <me@marioaguiar.net>
2020-03-16 20:16:54 -0600

    Use drawer instead of list

--- footer.php
+++ footer.php
@@ -13,25 +13,6 @@
 
 	</div><!-- #content -->
 
-	<aside class="mdc-drawer material-drawer mdc-drawer--modal">
-		<div class="mdc-drawer__header">
-			<h1 class="shrine-title"><?php bloginfo( 'name' ); ?></h1>
-		</div>
-		<div class="mdc-drawer__content">
-			<nav class="mdc-list">
-				<a class="mdc-list-item mdc-list-item--activated" aria-selected="true" tabindex="0" href="#">
-					<span class="mdc-list-item__text">Featured</span>
-				</a>
-				<a class="mdc-list-item" href="#">
-					<span class="mdc-list-item__text">Apartment</span>
-				</a>
-				<a class="mdc-list-item" href="#">
-					<span class="mdc-list-item__text">Accessories</span>
-				</a>
-			</nav>
-		</div>
-	</aside>
-
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'material-theme-wp' ) ); ?>">
