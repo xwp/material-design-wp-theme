d002e0e6b911a37781394d31e22d03f9dda464b5
Mario Aguiar <me@marioaguiar.net>
2020-03-16 20:38:29 -0600

    Close modal when clicking an item

--- assets/src/js/components/drawer.js
+++ assets/src/js/components/drawer.js
@@ -1,11 +1,28 @@
 import { MDCDrawer } from '@material/drawer';
 
-export const drawerInit = () => {
-	const drawerElement = document.querySelector( '.mdc-drawer' );
+const drawerElement = document.querySelector( '.mdc-drawer' );
 
+export const drawerInit = () => {
 	if ( ! drawerElement ) {
 		return;
 	}
 
 	return new MDCDrawer( drawerElement );
+};
+
+export const drawerHandler = ( topAppBar, drawer ) => {
+	topAppBar.listen( 'MDCTopAppBar:nav', () => {
+		drawer.open = !drawer.open;
+	} );
+
+	const listElement = drawerElement.querySelector( '.mdc-list' );
+	const mainContentElement = document.querySelector( '.site-content' );
+
+	listElement.addEventListener( 'click', event => {
+		drawer.open = false;
+	} );
+
+	document.body.addEventListener( 'MDCDrawer:closed', () => {
+		mainContentElement.querySelector( 'input, button' ).focus();
+	} );
 };
--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,13 +1,11 @@
 import { topAppBarInit } from './components/top-app-bar';
-import { drawerInit } from './components/drawer'
+import { drawerInit, drawerHandler } from './components/drawer'
 
 document.addEventListener( 'DOMContentLoaded', () => {
-	const topBarApp = topAppBarInit();
+	const topAppBar = topAppBarInit();
 	const drawer = drawerInit();
 
-	if ( topBarApp && drawer ) {
-		topBarApp.listen( 'MDCTopAppBar:nav', () => {
-			drawer.open = !drawer.open;
-		} );
+	if ( topAppBar && drawer ) {
+		drawerHandler( topAppBar, drawer );
 	}
 } );
