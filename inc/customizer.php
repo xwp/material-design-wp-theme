e5305db972539017dcc7eb3a5abf054b6b8d5a5c
Mario Aguiar <me@marioaguiar.net>
2020-03-19 10:07:28 -0600

    Copy functions from plugin to add controls

--- inc/customizer.php
+++ inc/customizer.php
@@ -43,6 +43,10 @@ function material_theme_wp_customize_register( $wp_customize ) {
 	}
 }
 
+function get_slug() {
+	return 'material';
+}
+
 /**
  * Render the site title for the selective refresh partial.
  *
@@ -102,7 +106,7 @@ function add_header_sections( $wp_customize ) {
 
 	$wp_customize->add_control( 'material_header_search_display',
 		[
-			'label' => esc_html__( 'Show search in header' ),
+			'label' => esc_html__( 'Show search in header', 'material-theme-wp' ),
 			'section' => 'material_header_section',
 			'priority' => 10,
 			'type' => 'checkbox',
@@ -115,7 +119,9 @@ function add_header_sections( $wp_customize ) {
 		]
 	);
 
-	$wp_customize->add_control(	maybe_us_image_radio_control( $wp_customize ) );
+	$wp_customize->add_control(	maybe_use_image_radio_control( $wp_customize ) );
+
+	add_color_controls( $wp_customize );
 }
 
 function get_image_radio_args() {
@@ -141,22 +147,211 @@ function get_image_radio_args() {
  * 
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function maybe_us_image_radio_control( $wp_customize ) {
+function maybe_use_image_radio_control( $wp_customize ) {
 	$args = get_image_radio_args();
 
 	if ( class_exists( 'MaterialThemeBuilder\Customizer\Image_Radio_Control' ) ) {
 		$image_radio_control = new \MaterialThemeBuilder\Customizer\Image_Radio_Control(
 			$wp_customize,
 			'material_header_layout',
-			get_image_radio_args()
+			$args
 		);
 	} else {
 		$image_radio_control = new Image_Radio_Control(
 			$wp_customize,
 			'material_header_layout',
-			get_image_radio_args()
+			$args
 		);
 	}
 
 	return $image_radio_control;
+}
+
+function get_color_palette_args() {
+	return [
+		'label'                => esc_html__( 'Background Color', 'material-theme-wp' ),
+		'section'              => 'colors',
+		'priority'             => 10,
+		'related_text_setting' => 'material_header_primary_background_color',
+		'related_setting'      => ! empty( $control['related_setting'] ) ? $control['related_setting'] : false,
+		'css_var'              => $control['css_var'],
+	];
+}
+
+function maybe_use_color_palette_control( $wp_customize ) {
+	/**
+	* Generate list of all the controls in the colors section.
+	 */
+	$controls = [];
+
+	if ( class_exists( 'MaterialThemeBuilder\Customizer\Material_Color_Palette_Control' ) ) {
+		foreach ( get_color_controls() as $control ) {
+			$controls[ $control['id'] ] = new \MaterialThemeBuilder\Customizer\Material_Color_Palette_Control(
+				$wp_customize,
+				prepend_slug( $control['id'] ),
+				[
+					'label'                => $control['label'],
+					'section'              => prepend_slug( '_header_section' ),
+					'related_text_setting' => ! empty( $control['related_text_setting'] ) ? $control['related_text_setting'] : false,
+					'related_setting'      => ! empty( $control['related_setting'] ) ? $control['related_setting'] : false,
+					'css_var'              => $control['css_var'],
+				]
+			);
+		}
+	} else {
+		foreach ( get_color_controls() as $control ) {
+			$controls[ $control['id'] ] = [
+				'label'                => $control['label'],
+				'section'              => prepend_slug( '_header_section' ),
+			];
+		}
+	}
+
+	add_controls( $wp_customize, $controls );
+
+	
+}
+
+function get_color_controls() {
+	return [
+		[
+			'id'                   => 'background_color',
+			'label'                => esc_html__( 'Bakground Color', 'material-theme-wp' ),
+			'related_text_setting' => prepend_slug( 'header_background_color' ),
+			'css_var'              => '--mdc-theme-header-primary',
+		],
+		[
+			'id'                   => 'text_color',
+			'label'                => esc_html__( 'Text Color', 'material-theme-wp' ),
+			'related_text_setting' => prepend_slug( 'header_text_color' ),
+			'css_var'              => '--mdc-theme-header-secondary',
+		],
+	];
+}
+
+function add_settings( $wp_customize, $settings = [] ) {
+	$slug = get_slug();
+
+	foreach ( $settings as $id => $setting ) {
+		$id = prepend_slug( $id );
+
+		if ( is_array( $setting ) ) {
+			$defaults = [
+				'capability'        => 'edit_theme_options',
+				'sanitize_callback' => 'sanitize_text_field',
+				'transport'         => 'postMessage',
+				'default'           => get_default( $id ),
+			];
+
+			$setting = array_merge( $defaults, $setting );
+		}
+
+		/**
+		 * Filters the customizer setting args.
+		 *
+		 * This allows other plugins/themes to change the customizer setting args.
+		 *
+		 * @param array   $setting Array of setting args.
+		 * @param string  $id      ID of the setting.
+		 */
+		$setting = apply_filters( $slug . '_customizer_setting_args', $setting, $id );
+
+		var_dump( $setting );
+
+		if ( is_array( $setting ) ) {
+			$wp_customize->add_setting(
+				$id,
+				$setting
+			);
+		} elseif ( $setting instanceof \WP_Customize_Setting ) {
+			$setting->id = $id;
+			$wp_customize->add_setting( $setting );
+		}
+	}
+}
+
+/**
+ * Prepend the slug name if it does not exist.
+ *
+ * @param  string $name The name of the setting/control.
+ * @return string
+ */
+function prepend_slug( $name ) {
+	$slug = get_slug();
+
+	return false === strpos( $name, "{$slug}_" ) ? "{$slug}_{$name}" : $name;
+}
+
+/**
+ * Get default value for a setting.
+ *
+ * @param  string $setting Name of the setting.
+ * @return mixed
+ */
+function get_default( $setting ) {
+	$slug = get_slug();
+	$setting  = str_replace( "{$slug}_", '', $setting );
+	$defaults   = get_default_values();
+
+	return isset( $defaults[ $setting ] ) ? $defaults[ $setting ] : '';
+}
+
+function get_default_values() {
+	return [
+		'background_color' => '#6200ee',
+		'text_color'       => '#03dac6',
+	];
+}
+
+function add_color_controls( $wp_customize ) {
+	/**
+	 * Generate list of all the settings in the colors section.
+	 */
+	$settings = [];
+
+	foreach ( get_color_controls() as $control ) {
+		$settings[ $control['id'] ] = [
+			'sanitize_callback' => 'sanitize_hex_color',
+		];
+	}
+
+	add_settings( $wp_customize, $settings );
+
+	maybe_use_color_palette_control( $wp_customize );
+}
+
+/**
+ * Add controls to customizer.
+ *
+ * @param  array $controls Array of controls to add to customizer.
+ * @return void
+ */
+function add_controls( $wp_customize, $controls = [] ) {
+	$slug = get_slug();
+
+	foreach ( $controls as $id => $control ) {
+		$id = prepend_slug( $id );
+
+		/**
+		 * Filters the customizer control args.
+		 *
+		 * This allows other plugins/themes to change the customizer controls args.
+		 *
+		 * @param array  $control Array of control args.
+		 * @param string $id      ID of the control.
+		 */
+		$control = apply_filters( $slug . '_customizer_control_args', $control, $id );
+
+		if ( is_array( $control ) ) {
+			$control['section'] = isset( $control['section'] ) ? prepend_slug( $control['section'] ) : '';
+			$wp_customize->add_control(
+				$id,
+				$control
+			);
+		} elseif ( $control instanceof \WP_Customize_Control ) {
+			$control->id      = $id;
+			$control->section = isset( $control->section ) ? prepend_slug( $control->section ) : '';
+			$wp_customize->add_control( $control );
+		}
+	}
 }
