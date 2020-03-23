b6950c04ad84f6c03371ea7d642c192a5212d8bb
Mario Aguiar <me@marioaguiar.net>
2020-03-23 10:52:31 -0600

    Re-structure to add settingsA

--- inc/customizer/footer.php
+++ inc/customizer/footer.php
@@ -7,6 +7,8 @@
 
 namespace MaterialTheme\Customizer\Footer;
 
+use MaterialTheme\Customizer;
+
 function add_section( $wp_customize ) {
 	$wp_customize->add_section( 'material_footer_section',
 		[
@@ -14,23 +16,54 @@ function add_section( $wp_customize ) {
 		]
 	);
 
-	$wp_customize->add_setting( 'material_footer_text',
-		[
-			'transport' => 'postMessage'
-		]
-	);
-
-	$wp_customize->add_control( 'material_footer_text',
-		[
-			'label'     => esc_html__( 'Footer text', 'material-theme-wp' ),
-			'section'   => 'material_footer_section',
-			'type'      => 'text',
-		]
-	);
+	add_settings( $wp_customize );
 }
 
 function render_text() {
 	$footer_text = get_theme_mod( 'material_footer_text', '&copy; 2020 Material.io' );
 
 	echo esc_html( $footer_text );
+}
+
+function add_controls( $wp_customize ) {
+	$controls = [];
+
+	foreach ( get_controls() as $control ) {
+		$controls[ $control[ 'id' ] ] = [
+			'label' => $control['label'],
+			'section' => Customizer\prepend_slug( 'footer_section' ),
+			'type'    => $control['type'],
+		];
+	}
+
+	Customizer\add_controls( $wp_customize, $controls );
+}
+
+function add_settings( $wp_customize ) {
+	$settings = [];
+
+	foreach ( get_controls() as $control ) {
+		$settings[ $control['id'] ] = [
+			'transport' => 'postMessage',
+		];
+	}
+
+	Customizer\add_settings( $wp_customize, $settings );
+
+	add_controls( $wp_customize );
+}
+
+function get_controls() {
+	return [
+		[
+			'id'                   => 'hide_back_to_top',
+			'label'                => esc_html__( 'Hide back to top button', 'material-theme-wp' ),
+			'type'                 => 'checkbox',
+		],
+		[
+			'id'                   => 'footer_text',
+			'label'                => esc_html__( 'Footer text', 'material-theme-wp' ),
+			'type'                 => 'text',
+		]
+	];
 }
