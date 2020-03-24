cd2994d9d89ee2c7ebb82c61991aa729c3a52cd6
Mario Aguiar <me@marioaguiar.net>
2020-03-24 11:38:22 -0600

    Fix mobile spacing

--- assets/js/front-end.asset.php
+++ assets/js/front-end.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f32f153afa6123a4bfdc49cd4279f1d0');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '36886d3878480d95d4d14e8942b8fe0d');
