6483d850e4fc54b155b6de30f3161dc51e9ccde9
Mario Aguiar <me@marioaguiar.net>
2020-03-31 13:58:33 -0600

    Update search color

--- assets/js/front-end.asset.php
+++ assets/js/front-end.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '5c152e156c6dfc3b208214a0ac748dc8');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'd1cb9ac716781c8e57106a9d62b34a2c');
