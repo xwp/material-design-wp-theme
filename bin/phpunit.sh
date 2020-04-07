e391643fec6762488967987e43de9cb9a059efe8
Ravi Chandra <ravichandrach1@gmail.com>
2020-04-07 16:52:20 +0530

    Update docker container name

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose --file=../../plugins/material-theme-builder/docker-compose.yml run --rm -u 1000 --workdir=/var/www/md/wp-content/themes/material-theme phpfpm -- composer test
+docker-compose --file=../../plugins/material-theme-builder/docker-compose.yml run --rm -u 1000 --workdir=/var/www/md/wp-content/themes/material-theme wordpress -- composer test
