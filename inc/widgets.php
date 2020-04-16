32012743b8579b0d7b3279bebe61bb986bd2a46e
Mario Aguiar <me@marioaguiar.net>
2020-04-16 11:47:54 -0500

    Update markup of archives

--- inc/widgets.php
+++ inc/widgets.php
@@ -14,6 +14,8 @@ namespace MaterialTheme\Widgets;
  */
 function setup() {
 	add_action( 'widgets_init', __NAMESPACE__ . '\replace_default_widgets' );
+	
+	add_filter( 'widget_archives_args', __NAMESPACE__ . '\build_archive' );
 }
 
 /**
@@ -25,4 +27,22 @@ function replace_default_widgets() {
 	unregister_widget( 'WP_Widget_Archives' );
 
 	register_widget( __NAMESPACE__ . '\WP_Widget_Archives' );
+}
+
+/**
+ * Replace container of archive's items
+ *
+ * @param  array $args Default arguments.
+ * @return array Updated arguments
+ */
+function build_archive( $args ) {
+	$new_args = [
+		'format' => 'custom',
+		'before' => '<li class="mdc-list-item"><span class="mdc-list-item__text">',
+		'after'  => '</span></li>',
+	];
+
+	$args = wp_parse_args( $new_args, $args );
+
+	return $args;
 }
