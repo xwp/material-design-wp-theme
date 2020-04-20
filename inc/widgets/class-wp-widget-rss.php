2a97dcf11b0f79cf4d5660b82d25b1ca91de32cc
Mario Aguiar <me@marioaguiar.net>
2020-04-20 10:33:14 -0500

    Use our own version of wp_widget_rss_output

--- inc/widgets/class-wp-widget-rss.php
+++ inc/widgets/class-wp-widget-rss.php
@@ -74,12 +74,119 @@ class WP_Widget_RSS extends \WP_Widget_RSS {
 		if ( $title ) {
 			echo wp_kses_post( $args['before_title'] . $title . $args['after_title'] );
 		}
-		wp_widget_rss_output( $rss, $instance );
+		$this->wp_widget_rss_output( $rss, $instance );
 		echo wp_kses_post( $args['after_widget'] );
 
 		if ( ! is_wp_error( $rss ) ) {
 			$rss->__destruct();
 		}
+		unset( $rss );
+	}
+
+	/**
+	 * Display the RSS entries in a list.
+	 *
+	 * @since 2.5.0
+	 *
+	 * @param string|array|object $rss RSS url.
+	 * @param array               $args Widget arguments.
+	 */
+	private function wp_widget_rss_output( $rss, $args = array() ) {
+		if ( is_string( $rss ) ) {
+			$rss = fetch_feed( $rss );
+		} elseif ( is_array( $rss ) && isset( $rss['url'] ) ) {
+			$args = $rss;
+			$rss  = fetch_feed( $rss['url'] );
+		} elseif ( ! is_object( $rss ) ) {
+			return;
+		}
+
+		if ( is_wp_error( $rss ) ) {
+			if ( is_admin() || current_user_can( 'manage_options' ) ) {
+				echo '<p><strong>' . esc_html__( 'RSS Error:', 'material-theme' ) . '</strong> ' . esc_html( $rss->get_error_message() ) . '</p>';
+			}
+			return;
+		}
+
+		$default_args = array(
+			'show_author'  => 0,
+			'show_date'    => 0,
+			'show_summary' => 0,
+			'items'        => 0,
+		);
+		$args         = wp_parse_args( $args, $default_args );
+
+		$items = (int) $args['items'];
+		if ( $items < 1 || 20 < $items ) {
+			$items = 10;
+		}
+		$show_summary = (int) $args['show_summary'];
+		$show_author  = (int) $args['show_author'];
+		$show_date    = (int) $args['show_date'];
+
+		if ( ! $rss->get_item_quantity() ) {
+			echo '<ul><li>' . esc_html__( 'An error has occurred, which probably means the feed is down. Try again later.', 'material-theme' ) . '</li></ul>';
+			$rss->__destruct();
+			unset( $rss );
+			return;
+		}
+
+		echo '<ul>';
+		foreach ( $rss->get_items( 0, $items ) as $item ) {
+			$link = $item->get_link();
+			while ( stristr( $link, 'http' ) != $link ) {
+				$link = substr( $link, 1 );
+			}
+			$link = esc_url( wp_strip_all_tags( $link ) );
+
+			$title = esc_html( trim( wp_strip_all_tags( $item->get_title() ) ) );
+			if ( empty( $title ) ) {
+				$title = esc_html__( 'Untitled', 'material-theme' );
+			}
+
+			$desc = html_entity_decode( $item->get_description(), ENT_QUOTES, get_option( 'blog_charset' ) );
+			$desc = esc_attr( wp_trim_words( $desc, 55, ' [&hellip;]' ) );
+
+			$summary = '';
+			if ( $show_summary ) {
+				$summary = $desc;
+
+				// Change existing [...] to [&hellip;].
+				if ( '[...]' == substr( $summary, -5 ) ) {
+					$summary = substr( $summary, 0, -5 ) . '[&hellip;]';
+				}
+
+				$summary = '<div class="rssSummary">' . esc_html( $summary ) . '</div>';
+			}
+
+			$date = '';
+			if ( $show_date ) {
+				$date = $item->get_date( 'U' );
+
+				if ( $date ) {
+					$date = ' <span class="rss-date">' . date_i18n( get_option( 'date_format' ), $date ) . '</span>';
+				}
+			}
+
+			$author = '';
+			if ( $show_author ) {
+				$author = $item->get_author();
+				if ( is_object( $author ) ) {
+					$author = $author->get_name();
+					$author = ' <cite>' . esc_html( wp_strip_all_tags( $author ) ) . '</cite>';
+				}
+			}
+
+			if ( '' === $link ) {
+				echo wp_kses_post( "<li>$title{$date}{$summary}{$author}</li>" );
+			} elseif ( $show_summary ) {
+				echo wp_kses_post( "<li><a class='rsswidget' href='$link'>$title</a>{$date}{$summary}{$author}</li>" );
+			} else {
+				echo wp_kses_post( "<li><a class='rsswidget' href='$link'>$title</a>{$date}{$author}</li>" );
+			}
+		}
+		echo '</ul>';
+		$rss->__destruct();
 		unset( $rss );
 	}
 }
