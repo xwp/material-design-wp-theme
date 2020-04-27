a6448ecde51a927a982fbc1ebc35dd286a420444
Mario Aguiar <me@marioaguiar.net>
2020-04-27 13:20:22 -0500

    Use our own version of post navigation / exclude VIP rules

--- single.php
+++ single.php
@@ -19,7 +19,7 @@ get_header();
 
 			get_template_part( 'template-parts/single', get_post_type() );
 
-			the_post_navigation();
+			get_template_part( 'template-parts/post-navigation' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
