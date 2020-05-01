67dd91fae01cebed9ade3c15c09d329d7ea6ee9e
Ravi Chandra <ravichandrach1@gmail.com>
2020-05-01 11:27:10 +0530

    Fix admin_url path

--- functions.php
+++ functions.php
@@ -187,30 +187,30 @@ function plugin_not_installed_notice() {
 	}
 	?>
 	<div class="notice notice-info is-dismissible  material-notice-container">
-		<img 
-			src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/plugin-icon.svg' ); ?>" 
-			alt="<?php _e( 'Material Theme Builder', 'material-theme' ); ?>" 
+		<img
+			src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/plugin-icon.svg' ); ?>"
+			alt="<?php esc_html_e( 'Material Theme Builder', 'material-theme' ); ?>"
 		/>
 
 		<div class="material-notice-container__content">
 			<h3 class="material-notice-container__content__title">
-				<?php 
-				esc_html_e( 
-					'Install the Material Plugin to customize your Material Theme', 
-					'material-theme' 
-				) 
+				<?php
+				esc_html_e(
+					'Install the Material Plugin to customize your Material Theme',
+					'material-theme'
+				)
 				?>
 			</h3>
 			<p class="material-notice-container__content__text">
-				<?php 
-				echo wp_kses( 
+				<?php
+				echo wp_kses(
 					sprintf(
 						/* translators: %s: url to the plugin installation page */
-						__( 
-							'To take full advantage of this theme you will need the Material Plugin. <a href="%s">Install and activate the plugin</a>', 
-							'material-theme' 
-						), 
-						esc_url( admin_url( '/wp-admin/plugin-install.php?s=Material+Theme+Builder&tab=search&type=term' ) )
+						__(
+							'To take full advantage of this theme you will need the Material Plugin. <a href="%s">Install and activate the plugin</a>',
+							'material-theme'
+						),
+						esc_url( admin_url( '/plugin-install.php?s=Material+Theme+Builder&tab=search&type=term' ) )
 					),
 					array( 'a' => array( 'href' => array() ) )
 				)
