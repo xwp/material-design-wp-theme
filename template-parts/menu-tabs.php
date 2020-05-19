6c05650c93dd06369be619a953e6542d602319ef
Ravi Chandra <ravichandrach1@gmail.com>
2020-05-19 23:29:06 +0530

    Update tab bar nav markup and styles to fix scrolling issue

--- template-parts/menu-tabs.php
+++ template-parts/menu-tabs.php
@@ -15,22 +15,24 @@ if ( ! has_nav_menu( 'menu-1' ) ) {
 
 ?>
 
-<div class="mdc-tab-bar tab-bar" role="tablist">
-	<div class="mdc-tab-scroller">
-		<div class="mdc-tab-scroller__scroll-area">
-			<div class="mdc-tab-scroller__scroll-content">
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-						'walker'         => new Menu_Walker(),
-						'container'      => '',
-						'items_wrap'     => '%3$s',
-						'depth'          => 1,
-					)
-				);
-				?>
+<div class="tab-bar__wrap">
+	<div class="mdc-tab-bar tab-bar" role="tablist">
+		<div class="mdc-tab-scroller">
+			<div class="mdc-tab-scroller__scroll-area">
+				<div class="mdc-tab-scroller__scroll-content">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+							'walker'         => new Menu_Walker(),
+							'container'      => '',
+							'items_wrap'     => '%3$s',
+							'depth'          => 1,
+						)
+					);
+					?>
+				</div>
 			</div>
 		</div>
 	</div>
