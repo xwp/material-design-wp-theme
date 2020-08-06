b6ffba8b420333f84afa53b87c59aa9b2b784764
Mario Aguiar <me@marioaguiar.net>
2020-08-06 13:54:05 -0500

    Update archive layout

--- archive.php
+++ archive.php
@@ -9,42 +9,55 @@
 
 get_header();
 
-$max_width  = get_theme_mod( 'material_archive_width', 'normal' );
-$class_name = sprintf( 'material-archive__%s', $max_width );
+$max_width    = get_theme_mod( 'material_archive_width', 'normal' );
+$class_name   = sprintf( 'material-archive__%s', $max_width );
+$content_span = is_active_sidebar( 'single' ) ? 'mdc-layout-grid__cell--span-8' : 'mdc-layout-grid__cell--span-12';
 ?>
 
-	<div id="primary" class="content-area <?php echo esc_attr( $class_name ); ?>">
-		<main id="main" class="site-main">
-
-		<?php
-		if ( have_posts() ) :
-			?>
-
-			<header class="page-header">
+	<main id="main"  class="mdc-layout-grid">
+		<div class="mdc-layout-grid__inner">
+			<header class="page-header mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
 				<?php
 				the_archive_title( '<h1 class="page-title mdc-typography mdc-typography--headline1">', '</h1>' );
 				the_archive_description( '<div class="archive-description mdc-typography mdc-typography--body1">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-
-			<div class="site-main__inner">
-				<?php get_template_part( 'template-parts/archive' ); ?>
-			</div>
 		</div>
 
-			<?php
-			get_template_part( 'template-parts/page-navigation' );
+		<div class="mdc-layout-grid__inner">
+			<div id="primary" class="content-area mdc-layout-grid__cell <?php echo esc_attr( $content_span ); ?> <?php echo esc_attr( $class_name ); ?>">
+				<div class="site-main">
 
-		else :
+					<?php
+					if ( have_posts() ) :
+						?>
 
-			get_template_part( 'template-parts/content', 'none' );
+						<div class="site-main__inner">
+							<?php get_template_part( 'template-parts/archive' ); ?>
+						</div>
 
-		endif;
-		?>
+						<?php
+						get_template_part( 'template-parts/page-navigation' );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					else :
 
+						get_template_part( 'template-parts/content', 'none' );
+
+					endif;
+					?>
+
+				</div><!-- #main -->
+			</div>
+
+			<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-4">
+				<?php
+				if ( is_active_sidebar( 'single' ) ) {
+					dynamic_sidebar( 'single' );
+				}
+				?>
+			</div>
+		</div>
+	</main><!-- #primary -->
 <?php
 get_sidebar();
 get_footer();
--- functions.php
+++ functions.php
@@ -135,9 +135,9 @@ function material_theme_wp_widgets_init() {
 
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Posts Sidebar', 'material-theme' ),
+			'name'          => esc_html__( 'Sidebar', 'material-theme' ),
 			'id'            => 'single',
-			'description'   => esc_html__( 'Widgets in single posts page', 'material-theme' ),
+			'description'   => esc_html__( 'Widgets in single posts and archive page', 'material-theme' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title mdc-typography--headline5">',
