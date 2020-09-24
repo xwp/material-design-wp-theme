0486de243fa8f843f1d2161a258c2491bcb22f8a
Rody Davis <31253215+rodydavis@users.noreply.github.com>
2020-09-24 12:36:17 -0700

    Updating metadata

--- template-parts/post-navigation.php
+++ template-parts/post-navigation.php
@@ -31,7 +31,7 @@ $next_link     = get_next_post_link( '%link' );
 <div class="post-navigation">
 	<div class="post-navigation__previous">
 		<?php if ( ! empty( $previous_link ) ) : ?>
-			<i class="material-icons mdc-button__icon">arrow_back_ios</i>
+			<i class="material-icons mdc-button__icon">chevron_left</i>
 			<?php echo $previous_link; // phpcs:ignore ?>
 		<?php endif; ?>
 	</div>
@@ -39,7 +39,7 @@ $next_link     = get_next_post_link( '%link' );
 	<div class="post-navigation__next">
 		<?php if ( ! empty( $next_link ) ) : ?>
 			<?php echo $next_link; // phpcs:ignore ?>
-			<i class="material-icons mdc-button__icon">arrow_forward_ios</i>
+			<i class="material-icons mdc-button__icon">chevron_right</i>
 		<?php endif; ?>
 	</div>
 </div>
