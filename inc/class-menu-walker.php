84b7d6c5e20c9093c36f175c112e1422f28a0139
Derek Herman <derek.herman@xwp.co>
2020-10-14 02:07:07 -0700

    Normalize copyright company name

--- inc/class-menu-walker.php
+++ inc/class-menu-walker.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Copyright 2020 Material Design
- * 
+ * Copyright 2020 Google LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * @package MaterialTheme
  */
 
@@ -57,7 +57,7 @@ class Menu_Walker extends \Walker {
 			$this->build_markup( $item )
 		);
 	}
-	
+
 	/**
 	 * Add necessary markup to build tab
 	 *
@@ -73,8 +73,8 @@ class Menu_Walker extends \Walker {
 			<span class="mdc-tab__text-label"><?php echo esc_html( $item->title ); ?></span>
 		</span>
 
-		<span 
-			class="mdc-tab-indicator 
+		<span
+			class="mdc-tab-indicator
 			<?php
 			if ( $is_active ) {
 				echo 'mdc-tab-indicator--active';
