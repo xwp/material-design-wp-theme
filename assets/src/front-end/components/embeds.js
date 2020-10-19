96117153b63b7dd398800a3d14cc89397b39020c
Ravi Chandra <ravichandrach1@gmail.com>
2020-10-19 20:22:39 +0530

    Add support for aspect ratio intrinsic embeds

---
+++ assets/src/front-end/components/embeds.js
@@ -0,0 +1,37 @@
+const makeFit = () => {
+	console.log( 'makeFit' );
+	document
+		.querySelectorAll( 'iframe, object, video' )
+		.forEach( function( video ) {
+			const container = video.parentNode;
+
+			// Skip videos we want to ignore.
+			if (
+				video.classList.contains( 'intrinsic-ignore' ) ||
+				video.parentNode.classList.contains( 'intrinsic-ignore' )
+			) {
+				return true;
+			}
+
+			if ( ! video.dataset.origwidth ) {
+				// Get the video element proportions.
+				video.setAttribute( 'data-origwidth', video.width );
+				video.setAttribute( 'data-origheight', video.height );
+			}
+
+			const iTargetWidth = container.offsetWidth;
+
+			// Get ratio from proportions.
+			const ratio = iTargetWidth / video.dataset.origwidth;
+
+			// Scale based on ratio, thus retaining proportions.
+			video.style.width = iTargetWidth + 'px';
+			video.style.height = video.dataset.origheight * ratio + 'px';
+		} );
+};
+
+export const embedsInit = () => {
+	makeFit();
+
+	window.addEventListener( 'resize', makeFit );
+};
